8e4cec6d52eb15c36180e421986446e1de5e4126
saya <sayaveth.souksaly@canada.ca>
2020-06-23 14:27:17 -0400

    test

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-<<<<<<< HEAD
 //import i18n from "./i18n";
-=======
-
-import i18n from "./i18n";
->>>>>>> dynamic-components
 import Buefy from "buefy";
 
 import UIkit from "uikit"; //remove later
@@ -24,12 +19,7 @@ UIkit.use(Icons); //remove later
 Vue.config.productionTip = false;
 
 new Vue({
-<<<<<<< HEAD
   //i18n,
-
   // i18n,
-=======
-  i18n,
->>>>>>> dynamic-components
   render: h => h(App)
 }).$mount("#app");
