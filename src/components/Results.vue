4a8a6283dffb007837e610cf2fecfebcb4da5a6c
saya <sayaveth.souksaly@canada.ca>
2020-06-25 15:49:47 -0400

    Print Result: Iteration 1

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -24,7 +24,9 @@
         </template>
         <template v-slot:footertext>
           <!-- <router-link to="/">{{ $t("restart") }}</router-link> || -->
-          <b-button class="card-footer-item">Print result</b-button>
+          <b-button class="card-footer-item" @click="printEntity"
+            >Print result</b-button
+          >
           <b-button class="card-footer-item"
             >Print all entities summary</b-button
           >
@@ -34,10 +36,50 @@
       </BaseCard>
     </div>
     <div class="column is-half"></div>
+
+    <b-modal :active.sync="isCardModalActive" full-screen class="print-modal">
+      <div class="columns">
+        <div class="column is-full">
+          <section class="section">
+            <div class="container">
+              <h1 class="title">Suggested Business Structure</h1>
+              <h2 class="subtitle">
+                {{ title }}
+              </h2>
+
+               <div><p>{{body}}</p></div>
+              <div>
+                <h2>{{ $t("advantages") }}</h2>
+                <ul>
+                  <span v-html="advantages"></span>
+                </ul>
+              </div>
+
+              <div>
+                <h2>{{ $t("disadvantages") }}</h2>
+                <ul>
+                  <span v-html="disadvantages"></span>
+                </ul>
+              </div>
+            </div>
+          </section>
+          <section class="section">
+            <div class="container">
+              <h1 class="title">Your Questions/Anwers</h1>
+              <h2 class="subtitle">
+                A simple container to divide your page into
+                <strong>sections</strong>, like the one you're currently reading
+              </h2>
+            </div>
+          </section>
+        </div>
+      </div>
+    </b-modal>
   </div>
 </template>
 <script>
 import BaseCard from "@/components/base-components/BaseCard.vue";
+
 export default {
   name: "Results",
   components: {
@@ -50,29 +92,40 @@ export default {
   data: function() {
     return {
       entity: {},
-      lang: "en"
+      lang: "en",
+      isCardModalActive: false
     };
   },
   created() {
     // get top Entity from data
-    this.entity = this.data.entities[this.entityId];
+    this.entity = this.data.entities[this.entityId] || {};
   },
   computed: {
     title: function() {
-      return this.entity[`title_${this.lang}`];
+      return this.entity[`title_${this.lang}`] || "N/A";
     },
 
     body: function() {
-      return this.entity[`summary_${this.lang}`];
+      return this.entity[`summary_${this.lang}`] || "N/A";
     },
 
     advantages: function() {
-      return this.entity[`advantage_${this.lang}`];
+      return this.entity[`advantage_${this.lang}`] || "N/A";
     },
 
     disadvantages: function() {
-      return this.entity[`disadvantage_${this.lang}`];
+      return this.entity[`disadvantage_${this.lang}`] || "N/A";
+    }
+  },
+  methods: {
+    printEntity: function() {
+      this.isCardModalActive = true;
     }
   }
 };
 </script>
+<style scoped>
+.print-modal {
+  z-index: 100000;
+}
+</style>
