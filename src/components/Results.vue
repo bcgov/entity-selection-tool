eff976048fa714dfae64332358bb25f4cdf55789
Manon <manon@MacBook-Pro.local>
2020-07-08 12:07:30 -0400

    summaries using json - starting point

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -134,12 +134,544 @@
         </footer>
       </div>
     </b-modal>
+    <!-- modal for summaries print view -->
+    <b-modal
+      :active.sync="isSummariesModalActive"
+      :can-cancel="canCancel"
+      full-screen
+      aria-role="dialog"
+      aria-modal
+      class="be-print-modal"
+    >
+      <div class="modal-card" style="width: auto">
+        <header class="modal-card-head be-print-modal-head">
+          <span class="be-modal-title">{{ $t("summaries") }}</span>
+        </header>
+        <section
+          id="printBodySummaries"
+          ref="PrintBody"
+          class="be-modal-print-body"
+        >
+          <h1 class="subtitle is-3">{{ $t("title") }}</h1>
+          <h2 class="subtitle is-4">
+            {{ data.entities.e1[`title_${this.langLocal}`] }}
+          </h2>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e1[`summury_desc_subtitle1_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e1[`summury_desc_subtitle1_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e1[`summury_desc_subtitle2_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e1[`summury_desc_subtitle2_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e1[`summury_desc_subtitle3_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e1[`summury_desc_subtitle3_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e1[`summury_desc_subtitle4_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e1[`summury_desc_subtitle4_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e1[`summury_desc_subtitle5_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e1[`summury_desc_subtitle5_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e1[`summury_desc_subtitle6_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e1[`summury_desc_subtitle6_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <hr />
+          <h2 class="subtitle is-4">
+            {{ data.entities.e2[`title_${this.langLocal}`] }}
+          </h2>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e2[`summury_desc_subtitle1_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e2[`summury_desc_subtitle1_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e2[`summury_desc_subtitle2_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e2[`summury_desc_subtitle2_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e2[`summury_desc_subtitle3_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e2[`summury_desc_subtitle3_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e2[`summury_desc_subtitle4_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e2[`summury_desc_subtitle4_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e2[`summury_desc_subtitle5_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e2[`summury_desc_subtitle5_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e2[`summury_desc_subtitle6_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e2[`summury_desc_subtitle6_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <hr />
+          <h2 class="subtitle is-4">
+            {{ data.entities.e3[`title_${this.langLocal}`] }}
+          </h2>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e3[`summury_desc_subtitle1_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e3[`summury_desc_subtitle1_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e3[`summury_desc_subtitle2_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e3[`summury_desc_subtitle2_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e3[`summury_desc_subtitle3_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e3[`summury_desc_subtitle3_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e3[`summury_desc_subtitle4_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e3[`summury_desc_subtitle4_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e3[`summury_desc_subtitle5_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e3[`summury_desc_subtitle5_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e3[`summury_desc_subtitle6_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e3[`summury_desc_subtitle6_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <hr />
+          <h2 class="subtitle is-4">
+            {{ data.entities.e4[`title_${this.langLocal}`] }}
+          </h2>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e4[`summury_desc_subtitle1_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e4[`summury_desc_subtitle1_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e4[`summury_desc_subtitle2_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e4[`summury_desc_subtitle2_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e4[`summury_desc_subtitle3_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e4[`summury_desc_subtitle3_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e4[`summury_desc_subtitle4_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e4[`summury_desc_subtitle4_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e4[`summury_desc_subtitle5_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e4[`summury_desc_subtitle5_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e4[`summury_desc_subtitle6_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e4[`summury_desc_subtitle6_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <hr />
+          <h2 class="subtitle is-4">
+            {{ data.entities.e5[`title_${this.langLocal}`] }}
+          </h2>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e5[`summury_desc_subtitle1_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e5[`summury_desc_subtitle1_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e5[`summury_desc_subtitle2_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e5[`summury_desc_subtitle2_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e5[`summury_desc_subtitle3_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e5[`summury_desc_subtitle3_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e5[`summury_desc_subtitle4_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e5[`summury_desc_subtitle4_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e5[`summury_desc_subtitle5_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e5[`summury_desc_subtitle5_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e5[`summury_desc_subtitle6_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e5[`summury_desc_subtitle6_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <hr />
+          <h2 class="subtitle is-4">
+            {{ data.entities.e6[`title_${this.langLocal}`] }}
+          </h2>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e6[`summury_desc_subtitle1_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e6[`summury_desc_subtitle1_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e6[`summury_desc_subtitle2_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e6[`summury_desc_subtitle2_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e6[`summury_desc_subtitle3_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e6[`summury_desc_subtitle3_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e6[`summury_desc_subtitle4_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e6[`summury_desc_subtitle4_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e6[`summury_desc_subtitle5_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e6[`summury_desc_subtitle5_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+          <div>
+            <h3 class="subtitle be-subtitle-summaries is-5">
+              {{
+                data.entities.e6[`summury_desc_subtitle6_h3_${this.langLocal}`]
+              }}
+            </h3>
+            <p>
+              {{
+                data.entities.e6[`summury_desc_subtitle6_p_${this.langLocal}`]
+              }}
+            </p>
+            <br />
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button" type="button" @click="print()">
+            {{ $t("print") }}
+          </button>
+          <button class="button" type="is-link">
+            <a
+              href="assets/EntityTypeLongDescriptionsFor-Profit.pdf"
+              target="_blank"
+            >
+              {{ $t("download") }}
+            </a>
+          </button>
+          <button
+            class="button"
+            type="button"
+            @click="isSummariesModalActive = false"
+          >
+            {{ $t("close") }}
+          </button>
+        </footer>
+      </div>
+    </b-modal>
   </div>
 </template>
 <script>
 import Vue from "vue";
 import BaseCard from "@/components/base-components/BaseCard.vue";
-import ModalSummaries from "./ModalSummaries.vue";
 import Printd from "printd";
 import VueI18nResults from "vue-i18n";
 
@@ -170,6 +702,8 @@ export default {
         questions_answers: "Questions/Anwers:",
         bold_italic: "bold and italic",
         text_answers: "Your answers are in {format}.",
+        summaries: "Summaries - Print View",
+        title: "Entity Type Long Descriptions (For-Profit)",
         print: "Print",
         download: "Download",
         close: "Close"
@@ -185,6 +719,8 @@ export default {
         questions_answers: "Questions/Réponses :",
         bold_italic: "gras et italique",
         text_answers: "Vos réponses sont en {format}.",
+        summaries: "Sommaires - Aperçu avant impression",
+        title: "Entity Type Long Descriptions (For-Profit) FR",
         print: "Imprimer",
         download: "Télécharger",
         close: "Fermer"
@@ -205,6 +741,7 @@ export default {
       entity: {},
       langLocal: this.lang,
       isCardModalActive: false,
+      isSummariesModalActive: false,
       canCancel: false,
       css: `.be-selected {
               font-weight: bolder;
@@ -242,13 +779,7 @@ export default {
       this.isCardModalActive = true;
     },
     printSummaries: function() {
-      this.$buefy.modal.open({
-        props: { lang: this.langLocal },
-        parent: this,
-        component: ModalSummaries,
-        hasModalCard: true,
-        fullScreen: true
-      });
+      this.isSummariesModalActive = true;
     },
     checkAnswer: function(questionIndex, answerIndex) {
       let userAnswerIndex = this.userAnswers[questionIndex].answerIndex;
